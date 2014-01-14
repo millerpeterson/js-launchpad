c93756fd4888a993bde78199467f5897429571e3
Miller Peterson <miller.peterson@gmail.com>
2014-01-13 22:35:34 -0500

    Add setLed Launchpad output method
    Signed-off-by: Miller Peterson <miller.peterson@gmail.com>

--- launchpad.js
+++ launchpad.js
@@ -1,8 +1,9 @@
+var _ = require('underscore');
 var util = require('util');
 var events = require('events');
 var midiInt = require('./node_midi_interface');
 
-function Launchpad() {
+var Launchpad = function() {
   events.EventEmitter.call(this);
 };
 
@@ -35,18 +36,53 @@ Launchpad.prototype.handleTopButtonPress = function(ccVal, midiVel) {
   var row = 0;
   var col = ccVal - 104; // Leftmost is 104
   this.emitButtonEvent(midiVel, row, col);
-}
+};
 
 Launchpad.prototype.handleNonTopButtonPress = function(noteVal, midiVel) {
   var row = Math.floor(noteVal / 16);
   row += 1; // Shift row down because top buttons are considered a row
   var col = noteVal % 16;
   this.emitButtonEvent(midiVel, row, col);
-}
+};
 
 Launchpad.prototype.cleanup = function() {
   this.midiInterface.cleanup();
-}
+};
+
+// var isValidSetLedArgs = function(row, col, red, green, mode) {
+//   return (
+//     (row >= 0) && (row <= 8) &&
+//     (col >= 0) && (col <= 8) &&
+//     !((row == 8) && (col == 8)) &&
+//     (red >= 0) && (red <= 3) &&
+//     (green >= 0) && (green <= 3) &&
+//     (_.contains(_.keys(setLedModes), mode))
+//   )
+// }
+
+Launchpad.prototype.setLed = function(row, col, color, mode) {
+  var red = color[0];
+  var green = color[1];
+  if (typeof(mode) === 'undefined') {
+    mode = 'copy';
+  }
+  var setLedModeFlag = {
+    'copy': 12,
+    'update': 8,
+    'flash': 0
+  }[mode];
+//  if !(isValidSetLedArgs)
+  outBytes = new Array(3);
+  if (row == 0) {
+    outBytes[0] = 176; // CC
+    outBytes[1] = 104 + col;
+  } else {
+    outBytes[0] = 144; // Note
+    outBytes[1] = ((row - 1) * 16) + col;
+  }
+  outBytes[2] = (green * 16) + red + setLedModeFlag;
+  this.midiInterface.sendBytes(outBytes);
+};
 
 var launchpad = Launchpad;
 module.exports.launchpad = launchpad;
