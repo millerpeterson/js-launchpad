4ce45a4696fbb36280da830095d1e6c92db1ab47
Miller Peterson <miller.peterson@gmail.com>
2014-02-07 21:31:31 -0500

    Don't try to auto-set port on node midi interface when we don't have a Launchpad port name
    Signed-off-by: Miller Peterson <miller.peterson@gmail.com>

--- node_midi_interface.js
+++ node_midi_interface.js
@@ -1,6 +1,7 @@
 var util = require('util');
 var events = require('events');
 var midi = require('midi');
+var _ = require('underscore');
 
 function NodeMIDIInterface() {
   events.EventEmitter.call(this);
@@ -12,12 +13,21 @@ NodeMIDIInterface.prototype.nodeMidiMessageReceived = function(deltaTime, messag
   this.bytesReceived(message);
 };
 
-NodeMIDIInterface.prototype.init = function() {
+NodeMIDIInterface.prototype.init = function(portRegex) {
   this.input = new midi.input();
   this.output = new midi.output();
-  this.portRegex = /Launchpad/;
-  this.input.openPort(this.findPort(this.input));
-  this.output.openPort(this.findPort(this.output));
+  if (typeof(portRegex === 'undefined')) {
+    portRegex = /Launchpad/;
+  }
+  this.portRegex = portRegex;
+  var inputPortNum = this.findPort(this.input);
+  if (inputPortNum >= 0) {
+    this.input.openPort(inputPortNum);
+  }
+  var outputPortNum = this.findPort(this.output);
+  if (outputPortNum >= 0) {
+    this.output.openPort(outputPortNum);
+  }
   this.input.on('message',
     this.nodeMidiMessageReceived.bind(this));
 };
@@ -28,6 +38,7 @@ NodeMIDIInterface.prototype.findPort = function(inOrOut) {
       return i;
     }
   }
+  return -1;
 };
 
 NodeMIDIInterface.prototype.bytesReceived = function(bytes) {
